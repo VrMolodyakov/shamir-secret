0d43a957d82352871f18fc1bbd4ca8b4c576d2b6
Vyacheslav M <vrmolodyakov@mail.ru>
2022-04-14 21:24:19 +0300

    json secret writing

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import algorithm.Splitter;
+import json.JsonHandler;
 import secret.ShamirSecret;
 
 import java.math.BigInteger;
@@ -8,6 +9,8 @@ public class Main {
     public static void main(String[] args) {
         Splitter splitter = new Splitter(7,4);
         List<ShamirSecret> shamirSecrets = splitter.splitIntoPieces(123);
-
+        ShamirSecret testSecret = new ShamirSecret(1,BigInteger.valueOf(123456));
+        JsonHandler handler = new JsonHandler();
+        handler.writeSecretPartToJson(testSecret,BigInteger.valueOf(98914),"parts/part.json");
     }
 }
--- src/json/JsonHandler.java
+++ src/json/JsonHandler.java
@@ -3,10 +3,13 @@ package json;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import secret.ShamirSecret;
 
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.math.BigInteger;
+import java.util.List;
 import java.util.Optional;
 
 public class JsonHandler {
@@ -29,11 +32,25 @@ public class JsonHandler {
             JSONObject jsonObject = (JSONObject) object;
             secret = BigInteger.valueOf((Long) jsonObject.get("secret"));
             primeNumber = Optional.ofNullable( (Long) jsonObject.get("P"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ParseException e) {
+        } catch (IOException | ParseException e) {
             e.printStackTrace();
         }
 
     }
+
+    public void writeSecretPartToJson(ShamirSecret part,BigInteger primeNumber, String path){
+        JSONObject secret = new JSONObject();
+        secret.put("point",part.getPart());
+        secret.put("value",part.getSecret());
+        secret.put("P",primeNumber);
+        try (FileWriter file = new FileWriter(path)) {
+            //We can write any JSONArray or JSONObject instance to the file
+            file.write(secret.toString());
+            file.flush();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
